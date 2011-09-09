8191b35dae12c49d9eccfb6d386ecb3158f9cfd8
Guille <guille@ggalmazor.com>
2011-09-09 18:41:04 +0200

    Katayunos 2011-2012 como para publicar

--- web/index.php
+++ web/index.php
@@ -7,24 +7,20 @@ $app = new Silex\Application;
 
 use Silex\Extension\TwigExtension;
 
-$app->register(new TwigExtension(), array(
-    'twig.path'       => ROOT_DIR.'/views',
-    'twig.class_path' => ROOT_DIR.'/vendor/Twig/lib',
-));
+$app->register(new TwigExtension(), array('twig.path' => ROOT_DIR . '/views', 'twig.class_path' => ROOT_DIR . '/vendor/Twig/lib',));
 
 $app->match('/', function () use($app) {
-  return $app['twig']->render('home.html');
-});
+    return $app['twig']->render('home.twig');
+  });
 
 $app->match('/humans.txt', function () use($app) {
-  return $app['twig']->render('robots.html');
-});
+    return $app['twig']->render('robots.twig');
+  });
 
-$app->error(function ($e) use ($app){
-  if ($e instanceof NotFoundHttpException) {
-    return new Response('', 302, array('Location' => '/'));
-  }
-  return new Response($app['twig']->render('home.html', array('e' => $e)), 500);
-});
+$app->error(function ($e) use ($app) {
+    if ($e instanceof NotFoundHttpException)
+      return new Response('', 302, array('Location' => '/'));
+    return new Response($app['twig']->render('home.twig', array('e' => $e)), 500);
+  });
 
 $app->run();
